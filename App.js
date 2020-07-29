faf721696504e3cc68710f9b3f991ce9dc3d02d6
athallahannafis <athallahannafis@gmail.com>
2020-07-29 16:29:49 +0700

    Implement different stack navigation

--- App.js
+++ App.js
@@ -4,14 +4,33 @@
  */
 // import 'react-native-gesture-handler';
 import React, {Component} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './src/screens/home';
+import SimpleCounter from './src/screens/simpleCounter';
+import RenderData from './src/screens/renderData';
+import Detail from './src/screens/detail';
+
 
 // Navigator
 import Navigator from './route.js';
 
+const stack = createStackNavigator();
 export default class App extends Component {
   render() {
+    // return (
+    //   <Navigator/>
+    // );
     return (
-      <Navigator/>
-    );
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen name="Home" component={Home}/>
+          <stack.Screen name="Simple Counter" component={SimpleCounter} />
+          <stack.Screen name="Render Data" component={RenderData} />
+          <stack.Screen name="Detail" component={Detail}/>
+        </stack.Navigator>
+      </NavigationContainer>
+    )
   };
 };
