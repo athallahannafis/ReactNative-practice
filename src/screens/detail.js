f76e85124b7b756c50e1e63d87e9ffc9cb947afe
athallahannafis <athallahannafis@gmail.com>
2020-07-29 16:32:15 +0700

    Remove unused imports

--- src/screens/detail.js
+++ src/screens/detail.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 import {globalStyling as gs} from '../styles/global-styling';
-import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 
 export default class Detail extends React.Component {
   constructor(props) {
