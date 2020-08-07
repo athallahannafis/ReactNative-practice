37116db88e051f1c33ea18c4ede3dc29f9441b68
athallahannafis <athallahannafis@gmail.com>
2020-08-07 23:51:41 +0700

    Implement bottom tab and major refactor

--- App.js
+++ App.js
@@ -7,25 +7,31 @@ import React, {Component} from 'react';
 import {NavigationContainer, DrawerActions} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator, DrawerItem} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-// Screens
-import Home from './src/screens/home';
-import SimpleCounter from './src/screens/simpleCounter';
-import RenderData from './src/screens/renderData';
-import Detail from './src/screens/detail';
+// Bottom tab Screens
+import HomeScreen from './src/bottomtab-screens/home';
+import SimpleCounterScreen from './src/bottomtab-screens/simpleCounter';
+import RenderDataScreen from './src/bottomtab-screens/renderData';
+import DetailScreen from './src/bottomtab-screens/detail';
+
+// Drawer screens
+import TestScreen from './src/drawer-screens/test';
+import Profile from './src/drawer-screens/profile';
 
 import { Text, View, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import {globalStyling as gs} from './src/styles/global-styling';
 
-const stack = createStackNavigator();
 const drawer = createDrawerNavigator();
+const bottomTab = createBottomTabNavigator();
 
 // Must make the stack for each parent feature
 const homeStack = createStackNavigator();
 const simpleCounterStack = createStackNavigator();
 const renderDataStack = createStackNavigator();
+const testStack = createStackNavigator();
 
 export default class App extends Component {
   constructor(props) {
@@ -35,6 +41,55 @@ export default class App extends Component {
       num: 0, 
     }
   }
+
+  CustomDrawer = (props) => {
+    return (
+      <View style={{
+        backgroundColor: "#7cde96"
+      }}>
+        <View style={[
+          gs.profileContainer, {
+            backgroundColor: "#7ee0ce"
+          }
+        ]}>
+          <Image source={require("./src/images/seele.jpg")}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 100,
+            marginBottom: 20,
+          }}
+          />
+          <Text>Seele Vollerei</Text>
+        </View>
+
+        {/* CUSTOM DRAWER ITEM */}
+        <TouchableOpacity style={gs.drawerItem}
+        onPress={() => props.navigation.navigate("bottom-tab")}>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={gs.drawerItem}
+        onPress={() => props.navigation.navigate("Simple Counter")}>
+          <Text>Simple Counter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={gs.drawerItem}
+        onPress={() => props.navigation.navigate("test-screen")}>
+          <Text>Test me!</Text>
+        </TouchableOpacity>
+
+        {/* LIBRARY DRAWER ITEM */}
+        {/* <DrawerItem
+        label="Home"
+        onPress={() => props.navigation.navigate("Home")}
+        />
+        <DrawerItem
+        label="Simple Counter"
+        onPress={() => props.navigation.navigate("Simple Counter")}
+        /> */}
+      </View>
+    )
+  }
+
   // DRAWER TOGGLER
   leftToggle = (props) => {
     return (
@@ -60,79 +115,90 @@ export default class App extends Component {
     )
   }
 
+  // BottomTab component
+  bottomTabComponent = (props) => {
+    return (
+      <bottomTab.Navigator
+      tabBarOptions={{
+        labelStyle:{
+          marginBottom: 10,
+          fontSize: 15,
+          fontWeight: "bold",
+        },
+        tabStyle: {
+          backgroundColor: "white",
+          borderstyle: "solid",
+          borderStartColor: "black"
+        },
+        activeBackgroundColor: "blue",
+      }}>
+        <bottomTab.Screen name="Home"
+        component={this.homeStackScreen}/>
+        <bottomTab.Screen name="Simple Counter"
+        component={this.counterStackScreen}/>
+        <bottomTab.Screen name="Render Data"
+        component={this.renderDataStackScreen}/>
+        <bottomTab.Screen name="test-screen"
+        component={this.testStackScreen} />
+      </bottomTab.Navigator>
+    )
+  }
+
   // ONE BY ONE
   /**
-  * I assume each feature has one stack, and sub feature from the parent feature
+  * Each feature has one stack, and sub feature from the parent feature
   * needs to be in one stack with the parent, for example in this render data
   * practice.
   */
-  homeNav = (props) => {
+  testStackScreen = (props) => {
+    return (
+      <testStack.Navigator>
+        <testStack.Screen
+        options={{
+          headerLeft: () => (this.leftToggle(props)),
+        }}
+        name="Test me!" component={TestScreen}/>
+      </testStack.Navigator>
+    )
+  }
+
+  homeStackScreen = (props) => {
     return (
       <homeStack.Navigator>
         <homeStack.Screen
         options={{
           headerLeft: () => (this.leftToggle(props)),
         }}
-        name="Home" component={Home}/>
+        name="Home" component={HomeScreen}/>
       </homeStack.Navigator>
     )
   }
-  simpleCounterNav = (props) => {
+  counterStackScreen = (props) => {
     return (
       <simpleCounterStack.Navigator>
         <simpleCounterStack.Screen
         options={{
           headerLeft: () => (this.leftToggle(props)),
         }}
-        name="Simple Counter" component={SimpleCounter} />
+        name="Simple Counter" component={SimpleCounterScreen} />
       </simpleCounterStack.Navigator>
     )
   }
 
-  /**
-  * Detail gets in this stack because it is part of Render Data
-  */
-  renderDataNav = (props) => {
+  
+  //Detail gets in this stack because it is part of Render Data
+  renderDataStackScreen = (props) => {
     return (
       <renderDataStack.Navigator>
         <renderDataStack.Screen
         options={{
           headerLeft: () => (this.leftToggle(props)),
         }}
-        name="Render Data" component={RenderData} />
-        <renderDataStack.Screen name="Detail" component={Detail} />
+        name="Render Data" component={RenderDataScreen} />
+        <renderDataStack.Screen name="Detail" component={DetailScreen} />
       </renderDataStack.Navigator>
     )
   }
-
-  CustomDrawer = (props) => {
-    return (
-      <View>
-        <View style={[
-          gs.profileContainer,
-        ]}>
-          <Image source={require("./src/images/seele.jpg")}
-          style={{
-            width: 60,
-            height: 60,
-            borderRadius: 100,
-            marginBottom: 20,
-          }}
-          />
-          <Text>Seele Vollerei</Text>
-        </View>
-        <DrawerItem
-        label="Home"
-        onPress={() => props.navigation.navigate("Home")}
-        />
-        <DrawerItem
-        label="Simple Counter"
-        onPress={() => props.navigation.navigate("Simple Counter")}
-        />
-      </View>
-    )
-  }
-
 
   render() {
     return (
@@ -149,12 +215,18 @@ export default class App extends Component {
           <stack.Screen name="Render Data" component={RenderData} />
           <stack.Screen name="Detail" component={Detail}/>
         </stack.Navigator> */}
-        <drawer.Navigator drawerContent={props => <this.CustomDrawer {...props} />}>
-          
-          <drawer.Screen name="Home" component={this.homeNav}/>
+        <drawer.Navigator
+        drawerContent={props => <this.CustomDrawer {...props} />}>
+          <drawer.Screen name="bottom-tab"
+          component={this.bottomTabComponent}/>
+          <drawer.Screen name="Home"
+          component={this.homeStackScreen}/>
           <drawer.Screen name="Simple Counter"
-          component={this.simpleCounterNav} />
-          <drawer.Screen name="Render Data" component={this.renderDataNav} />
+          component={this.counterStackScreen} />
+          <drawer.Screen name="Render Data"
+          component={this.renderDataStackScreen} />
+          <drawer.Screen name="test-screen"
+          component={this.testStackScreen}/>
         </drawer.Navigator>
       </NavigationContainer>
     )
